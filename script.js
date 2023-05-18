cceee8824803405ee32e73d0f1aac451fa67e372
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2023-05-18 14:10:04 -0400

    User input for second  gradient updating view

--- script.js
+++ script.js
@@ -10,5 +10,6 @@ color1.addEventListener("input", function () {
 });
 
 color2.addEventListener("input", function () {
-  console.log(color2.value);
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 });
