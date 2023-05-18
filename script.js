612742a0f1773d2b31a5696d107b0103d364ad74
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2023-05-18 14:36:45 -0400

    Functional random button for both user inputs

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomButton = document.getElementById("randomButton");
 body.style.background = "linear-gradient(to right, #00ff00 , #ff0000)";
 css.textContent = body.style.background;
+
+function generateRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
 
 function setGradient() {
   body.style.background =
@@ -12,6 +23,13 @@ function setGradient() {
   css.textContent = body.style.background;
 }
 
+function setRandom() {
+  color1.value = generateRandomColor();
+  color2.value = generateRandomColor();
+  setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+randomButton.addEventListener("click", setRandom);
